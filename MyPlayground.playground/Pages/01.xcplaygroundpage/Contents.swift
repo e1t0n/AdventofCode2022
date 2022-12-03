481b2af846e91941014a3d3cb4f4f874804f12ee
e1t0n <eltonlivera@gmail.com>
2022-12-03 18:12:36 +0000

    01 refactor

--- MyPlayground.playground/Pages/01.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/01.xcplaygroundpage/Contents.swift
@@ -4,42 +4,34 @@ let file = Bundle.main.url(forResource: "input", withExtension: "txt")!
 let content = try! String(contentsOf: file, encoding: .utf8)
 let allCalories = content.components(separatedBy: "\n")
 
-
 func part1() {
-    var most = 0
-    var elf = 0
-    for c in allCalories {
-        if let calorie = Int(c) {
-            elf += calorie
-        } else {
-            if most < elf {
-                most = elf
+    let total = allCalories
+        .split(separator: "")
+        .map {
+            $0.compactMap {
+                Int($0)
             }
-            elf = 0
+            .reduce(0, +)
         }
-    }
+        .max()
     
-    print(most)
+    print(total!)
 }
 
 func part2() {
-    var totalCalories = [Int]()
-    var elf = 0
-    for c in allCalories {
-        if let calorie = Int(c) {
-            elf += calorie
-        } else {
-            totalCalories.append(elf)
-            elf = 0
+    let total = allCalories
+        .split(separator: "")
+        .map {
+            $0.compactMap {
+                Int($0)
+            }
+            .reduce(0, +)
         }
-    }
-    
-    print(
-        totalCalories
         .sorted(by: >)
         .prefix(3)
         .reduce(0, +)
-    )
+    
+    print(total)
 }
 
 part1() //72602
